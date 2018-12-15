b3559565c13070a36cee1dba4bb2e84af74fe0f6
Graham Morley <graham@clicksco.com>
2018-12-15 15:41:21 +0000

    Branding elements

--- src/html.js
+++ src/html.js
@@ -13,18 +13,13 @@ export default class HTML extends React.Component {
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="#D0E0D8" />
           <meta name="apple-mobile-web-app-title" content="Lazywill" />
-          <link rel="apple-touch-icon" href="/icons/apple-icon-57x57.png" sizes="57x57" />
-          <link rel="apple-touch-icon" href="/icons/apple-icon-60x60.png" sizes="60x60" />
-          <link rel="apple-touch-icon" href="/icons/apple-icon-72x72.png" sizes="72x72" />
-          <link rel="apple-touch-icon" href="/icons/apple-icon-76x76.png" sizes="76x76" />
-          <link rel="apple-touch-icon" href="/icons/apple-icon-114x114.png" sizes="114x114" />
-          <link rel="apple-touch-icon" href="/icons/apple-icon-120x120.png" sizes="120x120" />
-          <link rel="apple-touch-icon" href="/icons/apple-icon-144x144.png" sizes="144x144" />
-          <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" sizes="152x152" />
-          <link rel="apple-touch-icon" href="/icons/apple-icon-180x180.png" sizes="180x180" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+
+          <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+          <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
