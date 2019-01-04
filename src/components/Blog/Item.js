7d0ed006eebdd05f43d620447a04313e6f6bd608
Graham Morley <graham@clicksco.com>
2019-01-04 12:59:15 +0000

    Temporarily remove post template to fix build

--- src/components/Blog/Item.js
+++ src/components/Blog/Item.js
@@ -1,4 +1,3 @@
-import Img from "gatsby-image";
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
--- src/templates/PostTemplate.js
+++ src/templates/PostTemplate.js
@@ -1,70 +1,70 @@
-import PropTypes from "prop-types";
-import React from "react";
-import { graphql } from "gatsby";
-require("prismjs/themes/prism-okaidia.css");
+// import PropTypes from "prop-types";
+// import React from "react";
+// import { graphql } from "gatsby";
+// require("prismjs/themes/prism-okaidia.css");
 
-import Seo from "../components/Seo";
-import Article from "../components/Article";
-import Post from "../components/Post";
-import { ThemeContext } from "../layouts";
+// import Seo from "../components/Seo";
+// import Article from "../components/Article";
+// import Post from "../components/Post";
+// import { ThemeContext } from "../layouts";
 
-const PostTemplate = props => {
-  const {
-    data: {
-      post,
-      authornote: { html: authorNote }
-    },
-    pageContext: { next, prev }
-  } = props;
+// const PostTemplate = props => {
+//   const {
+//     data: {
+//       post,
+//       authornote: { html: authorNote }
+//     },
+//     pageContext: { next, prev }
+//   } = props;
 
-  return (
-    <React.Fragment>
-      <ThemeContext.Consumer>
-        {theme => (
-          <Article theme={theme}>
-            <Post
-              post={post}
-              next={next}
-              prev={prev}
-              authornote={authorNote}
-              theme={theme}
-            />
-          </Article>
-        )}
-      </ThemeContext.Consumer>
+//   return (
+//     <React.Fragment>
+//       <ThemeContext.Consumer>
+//         {theme => (
+//           <Article theme={theme}>
+//             <Post
+//               post={post}
+//               next={next}
+//               prev={prev}
+//               authornote={authorNote}
+//               theme={theme}
+//             />
+//           </Article>
+//         )}
+//       </ThemeContext.Consumer>
 
-      <Seo data={post} />
-    </React.Fragment>
-  );
-};
+//       <Seo data={post} />
+//     </React.Fragment>
+//   );
+// };
 
-PostTemplate.propTypes = {
-  data: PropTypes.object.isRequired,
-  pageContext: PropTypes.object.isRequired
-};
+// PostTemplate.propTypes = {
+//   data: PropTypes.object.isRequired,
+//   pageContext: PropTypes.object.isRequired
+// };
 
-export default PostTemplate;
+// export default PostTemplate;
 
-//eslint-disable-next-line no-undef
-export const postQuery = graphql`
-  query PostBySlug($slug: String!) {
-  allContentfulPost{
-    edges{
-      node{
-        title
-        slug
-        excerpt {
-          excerpt
-        }
-        cover {
-          file {
-            url
-            fileName
-            contentType
-          }
-        }
-      }
-    }
-  }
-}
-`;
+// //eslint-disable-next-line no-undef
+// export const postQuery = graphql`
+//   query PostBySlug($slug: String!) {
+//   allContentfulPost{
+//     edges{
+//       node{
+//         title
+//         slug
+//         excerpt {
+//           excerpt
+//         }
+//         cover {
+//           file {
+//             url
+//             fileName
+//             contentType
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+// `;
