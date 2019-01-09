cb511511d3e5b8d21b66d0007bc50db3471754fe
Graham Morley <graham@clicksco.com>
2019-01-09 19:52:47 +0000

    Add public folder

---
+++ public/component---src-pages-404-js-7d3685c5ea96df9319b8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{297:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-7d3685c5ea96df9319b8.js.map
