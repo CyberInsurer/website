e2c6d4902c96d6e6cd8a20d9eb25ca68ea979d6f
Graham Morley <graham@clicksco.com>
2019-05-22 23:17:14 +0100

    Contact page changes started

---
+++ content/pages/2--policy/index.md
@@ -0,0 +1,5 @@
+---
+title: Our Policy
+---
+
+Key Words Document
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,40 @@
 import PropTypes from "prop-types";
 import React from "react";
-import { graphql } from "gatsby";
 import { ThemeContext } from "../layouts";
 import Article from "../components/Article";
 import Contact from "../components/Contact";
 import Headline from "../components/Article/Headline";
 import Seo from "../components/Seo";
+import Typed from 'typed.js';
+import Button from "antd/lib/button";
 
-const ContactPage = props => {
-  const {
+class ContactPage extends React.Component {
 
-  } = props;
+  componentDidMount() {
+
+    const options = {
+      strings: [
+        "Greetings! I'm Si.",
+        "How can I help you today?", 
+        "Si, at your service.",
+        "Choose from one of the options below..."
+      ],
+      typeSpeed: 45,
+      backSpeed: 30,
+      backDelay: 750,
+      loop: false
+    };
+    // this.el refers to the <span> in the render() method
+    this.typed = new Typed(this.el, options);
+  }
+
+  componentWillUnmount() {
+    // Make sure to destroy Typed instance on unmounting
+    // to prevent memory leaks
+    this.typed.destroy();
+  }
+
+  render() {
 
   return (
     <React.Fragment>
@@ -18,17 +42,45 @@ const ContactPage = props => {
         {theme => (
           <Article theme={theme}>
             <header>
-              <Headline title="Get in Touch" theme={theme} />
-              <p>Leave a message for one of our experts, and we’ll get back to you as soon as possible.</p>
+              <Headline title="Contact Us" theme={theme} />
+              <h2><span
+                style={{ whiteSpace: 'pre' }}
+                ref={(el) => { this.el = el; }}
+              /></h2>
             </header>
-            <Contact theme={theme} />
+            <div className="space">
+              <Button className="ant-tab" type="primary" htmlType="submit">
+                Get a a Quote
+              </Button>
+              <Button className="ant-tab" type="primary" htmlType="submit">
+                Make a Claim
+              </Button>
+              <Button className="ant-tab" type="primary" htmlType="submit">
+                Something Else?
+              </Button>
+            </div>
+            <div className="space">
+              <Contact theme={theme} />
+            </div>
           </Article>
         )}
       </ThemeContext.Consumer>
 
       <Seo />
+      {/* --- STYLES --- */}
+      <style jsx>{`
+        .space {
+          margin: 30px 0;
+        }
+        .ant-tab {
+          margin-right: 4px;
+          padding: 4px;
+        }
+      `}</style>
     </React.Fragment>
-  );
+    );
+  };
+
 };
 
 ContactPage.propTypes = {
