889b013ec6121f201526086bfc79b8a98324393c
Graham Morley <graham@clicksco.com>
2019-09-21 18:56:24 +0100

    QBE Content Updates

--- src/components/Contact/Complaints.js
+++ src/components/Contact/Complaints.js
@@ -8,8 +8,8 @@ const Complaints = props => {
         <React.Fragment>
             <div className="tab-content">
                 <p class="intro">We’re really sorry if it feels like we have let you down and whatever it is we want to put it right, quickly with no fuss and in a way that makes you happy.</p>
-                <p>Our policy is underwritten by QBE, one of the largest insurers and reinsurers in the World. We’d like you to be able to speak with them directly to ensure independent, impartial and professional resolution of your complaint. </p>
-                <p>You can reach QBE by:</p>
+                <p>Our policy is underwritten by QBE Underwriting Limited, one of the largest insurers and reinsurers in the World. We’d like you to be able to speak with them directly to ensure independent, impartial and professional resolution of your complaint. </p>
+                <p>You can reach QBE UK Limited by:</p>
                 <dl>
                     <dt>Calling</dt>
                     <dd>020 7105 5988</dd>
@@ -26,6 +26,11 @@ const Complaints = props => {
                 </dl>
                 <p>Thanks</p>
                 <p>Si.</p>
+                <p>
+                    <small>If eligible, you can contact the UK FOS via its website: <a href="http://www.financial-ombudsman.org.uk/consumer/complaints.htm" target="_blank">http://www.financial-ombudsman.org.uk/consumer/complaints.htm</a>, write to the UK FOS, Exchange Tower, London E14 9SR, or telephone 0300 123 9 123 or 0800 023 4567.
+Where applicable you must refer its complaint to the UK FOS (a) within six (6) months of our final response letter or (b) when we have failed to resolve the issue within eight (8) weeks.
+The UK FOS provides impartial advice free of charge and contacting them will not affect your legal rights</small>
+                </p>
             </div>
             {/* --- STYLES --- */}
             <style jsx>{`
